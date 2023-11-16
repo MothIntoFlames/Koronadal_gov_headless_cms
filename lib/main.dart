4f9a4bec1734814950fa845ca141d02bbe0cc2b8
MothIntoFlames <mcscasicas@addu.edu.ph>
2023-11-16 11:13:16 +0800

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,126 +1,120 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:wordpress_client/wordpress_client.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+//class MyApp extends StatelessWidget {
+// @override
+// Widget build(BuildContext context) {
+//   return MaterialApp(
+//   title: 'Flutter WordPress App',
+//    theme: ThemeData(
+//      primarySwatch: Colors.blue,
+///    ),
+//     home: HomePage(),
+//    );
+//  }
+//}
 
-  // This widget is the root of your application.
+//class Post {
+// final String title;
+// final String content;
+
+// Post({required this.title, required this.content});
+
+// factory Post.fromJson(Map<String, dynamic> json) {
+//   return Post(
+//     title: json['title']['rendered'],
+//  content: json['content']['rendered'],
+//  );
+// }
+//}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final String wordpressUrl = 'https://koronadal.gov.ph/wp-json/wp/v2//posts';
+  late List<Post> myPosts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPosts();
+  }
+
+  Future<void> fetchPosts() async {
+
+    var request = ListPostRequest(page: 1, perPage: 20, status: ContentStatus.publish);
+
+    final wpResponse - await WP_Api().myclient.posts.list(request);
+
+
+
+    final result = wpResponse.map(
+      onSuccess: (response) {
+
+        return response.data;
+      },
+      onFailure:(response) {
+        print(response.error.toString());
+        return <Posts> [];
+      },
     );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+    List<Posts> wpPostsArr = [];
+    result.forEach((element) {
+      Posts dposts = Posts(
+        id: element.id.toString(),
+        title: element.title?.parsedText.toString() ?? "",
+        content: element.content?.parsedText.toString() ?? "",
+      );
+      wpPostsArr.add(dposts);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
     });
-  }
+
+    setState(() {
+      allposts = wpPostsArr;
+    });
+    //final response = await http.get(Uri.parse(wordpressUrl));
+
+    //if (response.statusCode == 200) {
+    //  List<dynamic> data = json.decode(response.body);
+     // setState(() {
+     //   posts = data.map((post) => Post.fromJson(post)).toList();
+    //  });
+   // } else {
+   //   throw Exception('Failed to load posts');
+   // }
+//  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Flutter WordPress App'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: posts != null
+          ? ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(posts[index].title),
+                  subtitle: Text(posts[index].content),
+                  // Customize the display of other post information as needed
+                );
+              },
+            )
+          : Center(
+              child: CircularProgressIndicator(),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }

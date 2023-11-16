4c4ada000aa5281d60526481e607c31c532fb476
HarrisonLu78 <hflu@addu.edu.ph>
2023-11-16 11:19:47 +0800

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,126 +1,171 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:wordpress_client/wordpress_client.dart';
 
-void main() {
-  runApp(const MyApp());
+import './models/posts.dart';
+import './services/wpapi.dart';
+import './widgets/card.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => const HomePage(),
+      },
+    ));
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class _HomePageState extends State<HomePage> {
+  List<Posts> allPosts = [];
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  void initState() {
+    super.initState();
+    getPostDataWP();
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  Future<void> getPostDataWP() async {
+    var request =
+        ListPostRequest(page: 1, perPage: 20, status: ContentStatus.publish);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+    final wpResponse = await WP_Api().myclient.posts.list(request);
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+// Dart 3 style
+// switch (wpResponse) {
+//     case WordpressSuccessResponse():
+//       final data = wpResponse.data; // List<Post>
+//       break;
+//     case WordpressFailureResponse():
+//       final error = wpResponse.error; // WordpressError
+//       break;
+// }
 
-  final String title;
+// or
+// wordpress_client style
+    final result = wpResponse.map(
+      onSuccess: (response) {
+        // print(response.message);
+        // print(response.data.length.toString());
+        // print(response.data[0].title?.parsedText.toString());
+        return response.data;
+      },
+      onFailure: (response) {
+        print(response.error.toString());
+        return <Post>[];
+      },
+    );
+    List<Posts> wpPostsArr = [];
+    result.forEach((element) {
+      Posts dposts = Posts(
+        id: element.id.toString(),
+        title: element.title?.parsedText.toString() ?? "",
+        content: element.content?.parsedText.toString() ?? "",
+      );
+      wpPostsArr.add(dposts);
+    });
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      allPosts = wpPostsArr;
     });
   }
 
-  @override
+  List<Posts> wpPostsArr = [];
+
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        body: SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Column(
+        children: [
+          Stack(
+            children: <Widget>[
+              Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 700,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              "https://static.wixstatic.com/media/dc9771_fb7844201fe5414699f6e344dd95ba74~mv2.gif/v1/fill/w_1600,h_900,al_c,q_90/file.jpg"),
+                          fit: BoxFit.cover))),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Text(
+                          "GovPh",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 25),
+                        ),
+                      ),
+                      Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Icon(Icons.ac_unit_outlined),
+                      )
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Department",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 70,
+                              fontWeight: FontWeight.w700),
+                        ),
+                      ),
+                      Text(
+                        "of",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 70,
+                            fontWeight: FontWeight.w700),
+                      ),
+                      Text(
+                        "Tourism",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 70,
+                            fontWeight: FontWeight.w700),
+                      )
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * .5,
+            height: 512,
+            child: Expanded(
+                child: allPosts.isNotEmpty
+                    ? ListView.builder(
+                        scrollDirection: Axis.vertical,
+                        itemCount: allPosts.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          if (allPosts.isNotEmpty) {
+                            return MyCard(post: allPosts[index]);
+                          } else
+                            return LinearProgressIndicator();
+                        })
+                    : Center(child: CircularProgressIndicator())),
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    ));
   }
 }
---
+++ lib/widgets/card.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import '/models/posts.dart';
+
+class MyCard extends StatelessWidget {
+  final Posts post;
+
+  const MyCard({Key? key, required this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: SizedBox(
+        width: 300,
+        height: 300,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                post.title,
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 25,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                post.content,
+                maxLines: 7,
+                style: TextStyle(
+                  fontWeight: FontWeight.w200,
+                  fontSize: 11,
+                ),
+              ),
+            ),
+            TextButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.panorama_fish_eye),
+                label: Text("Read Article"))
+          ],
+        ),
+      ),
+    );
+  }
+}
